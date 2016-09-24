32512754dcf64799a9380c039e2b43b7747246f8
Sápi Sándor <sandorsapi@e-development.hu>
2016-09-25 01:50:22 +0200

    Revert "family-at"
    This reverts commit 8cdb21afb5b351232a6835c100c0adcc93a12a98.

--- Bridge_App/Bridge_App/EvaluationAndSolution.cs
+++ Bridge_App/Bridge_App/EvaluationAndSolution.cs
@@ -81,6 +81,13 @@ namespace Bridge_App
                     this.progressStep++;
                 }
 
+                if (step == 3)
+                {
+                    MovedTwoMemberMax(this.leftMembers, this.rightMembers);
+                    this.step++;
+                    this.progressStep++;
+                }
+
                 //More moved
                 if (this.step % 2 >= 1)
                 {
